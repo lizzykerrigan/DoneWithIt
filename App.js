b712220ea314ed4ac53175e1314dae55ac4b8a68
lizzykerrigan <lizzykerrigan@outlook.com>
2020-06-20 21:30:28 +0100

    change app.js to MessagesScreen

--- App.js
+++ App.js
@@ -2,9 +2,10 @@ import React from "react";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
+import MessagesScreen from "./app/screens/MessagesScreen";
 
 export default function App() {
-  return <ViewImageScreen />;
+  return <MessagesScreen />;
 }
 
 // const styles = StyleSheet.create({
