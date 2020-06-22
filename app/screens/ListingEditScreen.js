d6472fae3ba34f9544838f513b484bb36f94296a
lizzykerrigan <lizzykerrigan@outlook.com>
2020-06-22 14:21:58 +0100

    update ListingEditScreen

--- app/screens/ListingEditScreen.js
+++ app/screens/ListingEditScreen.js
@@ -5,21 +5,38 @@ import * as Yup from "yup";
 import { Form, FormField, FormPicker, SubmitButton } from "../components/forms";
 import ScreenComponent from "../components/ScreenComponent";
 import CategoryPickerItem from "../components/CategoryPickerItem";
+import FormImagePicker from "../components/forms/FormImagePicker/FormImagePicker";
+import useLocation from "../hooks/useLocation";
 
 const validationSchema = Yup.object().shape({
   title: Yup.string().required().min(1).label("Title"),
   price: Yup.number().required().min(1).max(10000).label("Price"),
   description: Yup.string().label("Description"),
   category: Yup.object().required().nullable().label("Category"),
+  images: Yup.array().min(1, "Please select at least one image."),
 });
 
 const categories = [
-  { label: "Furniture", value: 1, backgroundColor: "red", icon: "apps" },
-  { label: "Clothing", value: 2, backgroundColor: "green", icon: "email" },
-  { label: "Tech", value: 3, backgroundColor: "blue", icon: "lock" },
+  {
+    backgroundColor: "#fc5c65",
+    icon: "floor-lamp",
+    label: "Furniture",
+    value: 1,
+  },
+  { backgroundColor: "#fd9644", icon: "car", label: "Cars", value: 2 },
+  { backgroundColor: "#fed330", icon: "camera", label: "Cameras", value: 3 },
+  { backgroundColor: "#26de81", icon: "cards", label: "Games", value: 4 },
+  {
+    backgroundColor: "#2bcbba",
+    icon: "shoe-heel",
+    label: "Clothing",
+    value: 5,
+  },
 ];
 
 const ListingEditScreen = () => {
+  const location = useLocation();
+
   return (
     <ScreenComponent style={styles.container}>
       <Form
@@ -28,10 +45,12 @@ const ListingEditScreen = () => {
           price: "",
           description: "",
           category: null,
+          images: [],
         }}
-        onSubmit={(values) => console.log(values)}
+        onSubmit={(values) => console.log(location)}
         validationSchema={validationSchema}
       >
+        <FormImagePicker name="images" />
         <FormField maxLength={255} name="title" placeholder="Title" />
         <FormField
           keyboardType="numeric"
