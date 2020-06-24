7a0bc7fc6321ed0c1235776f8d329ea47c9b49b2
lizzykerrigan <lizzykerrigan@outlook.com>
2020-06-24 19:49:06 +0100

    render NavigationContainer in App.js

--- App.js
+++ App.js
@@ -1,30 +1,13 @@
-import React, { useEffect, useState } from "react";
-import * as ImagePicker from "expo-image-picker";
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
 
-import ListingEditScreen from "./app/screens/ListingEditScreen";
-import ScreenComponent from "./app/components/ScreenComponent";
-import { Button, Image } from "react-native";
-import ImageInput from "./app/components/ImageInput/ImageInput";
-import ImageInputList from "./app/components/ImageInputList";
+import navigationTheme from "./app/navigation/navigationTheme";
+import AppNavigator from "./app/navigation/AppNavigator";
 
 export default function App() {
-  const [imageUris, setImageUris] = useState([]);
-
-  const handleAdd = (uri) => {
-    setImageUris([...imageUris, uri]);
-  };
-
-  const handleRemove = (uri) => {
-    setImageUris(imageUris.filter((element) => element !== uri));
-  };
-
   return (
-    <ScreenComponent>
-      <ImageInputList
-        imageUris={imageUris}
-        onAddImage={handleAdd}
-        onRemoveImage={handleRemove}
-      />
-    </ScreenComponent>
+    <NavigationContainer theme={navigationTheme}>
+      <AppNavigator />
+    </NavigationContainer>
   );
 }
