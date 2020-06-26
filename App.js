866554fe885227548c5ddd0cf5fa2a1638254504
lizzykerrigan <lizzykerrigan@outlook.com>
2020-06-26 18:25:14 +0100

    add netInfo

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import React from "react";
+import NetInfo, { useNetInfo } from "@react-native-community/netinfo";
 import { NavigationContainer } from "@react-navigation/native";
 
 import navigationTheme from "./app/navigation/navigationTheme";
 import AppNavigator from "./app/navigation/AppNavigator";
 
 export default function App() {
+  const netInfo = useNetInfo();
+  console.log(netInfo.isInternetReachable);
+
   return (
     <NavigationContainer theme={navigationTheme}>
       <AppNavigator />
